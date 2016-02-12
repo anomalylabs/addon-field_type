2536f10f9c79947ff392aaffb9d14704ceba630c
Ryan Thompson <ryan@pyrocms.com>
2016-02-12 09:35:52 -0600

    Select2

---
+++ resources/js/input.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    // Initialize form switches
+    $('.addon-field_type select').each(function () {
+        $(this).select2({
+            theme: 'bootstrap'
+        });
+    });
+});
